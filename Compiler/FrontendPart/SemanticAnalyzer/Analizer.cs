94dc403d85c12f5968a2934f08a88e4c8642cc97
prokop7 <a.prokopev@innopolis.ru>
2017-11-17 01:49:38 +0300

    Added Simple Inheritance.

--- Compiler/FrontendPart/SemanticAnalyzer/Analizer.cs
+++ Compiler/FrontendPart/SemanticAnalyzer/Analizer.cs
@@ -7,14 +7,13 @@ using Compiler.TreeStructure.Expressions;
 
 namespace Compiler.FrontendPart.SemanticAnalyzer
 {
-    // TODO Stages of analizer
-    // Fill class table
-    // Fill variable table for classes
-    // Fill variable table for methods and check initialization of variables
-    // Generic inheritance
-    // Replace Generic classes with existing
-    // Check types (should be expanded)
-    
+    /// TODO Stages of analizer
+    /// [??%] Fill class table    <see cref="FillStaticTable"/>
+    /// [??%] Fill variable/methods table for classes    <see cref="FillMethodsTable"/>
+    /// [90%] Simple inheritance    <<see cref="AddInheritedMembers"/>
+    /// [80%] Fill variable table for methods and check initialization of variables    <see cref="VariableDeclarationCheck"/>
+    /// [ 0%] Replace Generic classes with existing
+    /// [10%] Check types (should be expanded)]
     public class Analizer
     {
         private readonly List<Class> _classList;
@@ -28,6 +27,8 @@ namespace Compiler.FrontendPart.SemanticAnalyzer
         {
             FillStaticTable();
             FillMethodsTable();
+            AddInheritedMembers();
+            VariableDeclarationCheck();
             return _classList;
         }
 
@@ -41,6 +42,26 @@ namespace Compiler.FrontendPart.SemanticAnalyzer
             throw new System.NotImplementedException();
         }
 
+        private void AddInheritedMembers()
+        {
+            foreach (var @class in _classList)
+            {
+                AddParentMethods(@class);
+            }
+
+            void AddParentMethods(Class @class)
+            {
+                if (@class.Base == null)
+                    return;
+                AddParentMethods(@class);
+                var members = @class.Members;
+                foreach (var (key, pair) in @class.Base.Members)
+                    if (!members.ContainsKey(key))
+                        members.Add(key, pair);
+            }
+        }
+
+
         public void VariableDeclarationCheck()
         {
             var visitor = new VariableDeclarationChecker();
@@ -50,7 +71,5 @@ namespace Compiler.FrontendPart.SemanticAnalyzer
             }
             Console.WriteLine("Variable declaration checking is Done");
         }
-        
-        
     }
 }
