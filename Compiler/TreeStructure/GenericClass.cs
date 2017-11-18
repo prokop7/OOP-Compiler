67b596821924960682091c1c0ab53a0ed5e096d9
prokop7 <a.prokopev@innopolis.ru>
2017-11-18 16:00:22 +0300

    Added GenericClass

---
+++ Compiler/TreeStructure/GenericClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+namespace Compiler.TreeStructure
+{
+    public class GenericClass : Class
+    {
+        public GenericClass(ClassName name) : base(name)
+        {
+            SelfClassName = name;
+        }
+
+        public Dictionary<ClassName, List<ICommonTreeInterface>> References { get; set; } =
+            new Dictionary<ClassName, List<ICommonTreeInterface>>();
+    }
+}
